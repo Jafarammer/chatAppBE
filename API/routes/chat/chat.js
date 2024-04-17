fb312220ab6c8da082681f69644d5fa5535e11c8
jafarammer <jafalkaff96@gmail.com>
2024-04-17 22:52:13 +0700

    rename group chat

--- API/routes/chat/chat.js
+++ API/routes/chat/chat.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   accessChat,
   createGroupChat,
+  renameGroupChat,
 } = require("../../controller/chatController");
 const { protect } = require("../../middleware/authMiddleware");
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/", protect, accessChat);
 router.post("/group", protect, createGroupChat);
+router.put("/rename", protect, renameGroupChat);
 
 module.exports = router;
