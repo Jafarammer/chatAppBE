19531a46c2f87c73d7775b0293106583fda05714
jafarammer <jafalkaff96@gmail.com>
2024-04-17 23:10:23 +0700

    add user to group & remove user from group

--- API/controller/chatController.js
+++ API/controller/chatController.js
@@ -125,4 +125,57 @@ const renameGroupChat = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { accessChat, fetchChats, createGroupChat, renameGroupChat };
+const addToGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const added = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!added) {
+    res.status(404);
+    throw new Error("Chat not found");
+  } else {
+    res.json(added);
+  }
+});
+
+const removeFromGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!removed) {
+    res.status(404);
+    throw new Error("Chat not found");
+  } else {
+    res.json(removed);
+  }
+});
+
+module.exports = {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroupChat,
+  addToGroup,
+  removeFromGroup,
+};
--- API/routes/chat/chat.js
+++ API/routes/chat/chat.js
@@ -3,6 +3,8 @@ const {
   accessChat,
   createGroupChat,
   renameGroupChat,
+  addToGroup,
+  removeFromGroup,
 } = require("../../controller/chatController");
 const { protect } = require("../../middleware/authMiddleware");
 
@@ -11,5 +13,7 @@ const router = express.Router();
 router.post("/", protect, accessChat);
 router.post("/group", protect, createGroupChat);
 router.put("/rename", protect, renameGroupChat);
+router.put("/groupadd", protect, addToGroup);
+router.put("/groupremove", protect, removeFromGroup);
 
 module.exports = router;
