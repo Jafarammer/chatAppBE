8aaa7956e45c6287d16919595d5e11e1d8641194
jafarammer <jafalkaff96@gmail.com>
2024-04-17 22:39:13 +0700

    create group chat

--- API/routes/chat/chat.js
+++ API/routes/chat/chat.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { accessChat } = require("../../controller/chatController");
+const {
+  accessChat,
+  createGroupChat,
+} = require("../../controller/chatController");
 const { protect } = require("../../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", protect, accessChat);
+router.post("/group", protect, createGroupChat);
 
 module.exports = router;
