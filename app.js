18134d95a98238204fb9ff6cdbe1f5cb7978a9a8
jafarammer <jafalkaff96@gmail.com>
2024-04-17 21:56:18 +0700

    create chat

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const userRoutes = require("./API/routes/user/user");
+const chatRoutes = require("./API/routes/chat/chat");
 const findUser = require("./API/routes/user/find");
 const { notFound, errorHandler } = require("./API/middleware/errorHandling");
 
@@ -9,6 +10,8 @@ app.use(express.json());
 // user routes
 app.use("/user", userRoutes);
 app.use("/user", findUser);
+// chat routes
+app.use("/chat", chatRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
